d146d927572f14a295471a99d7ee9b9405b1dab2
NickBarkerDev <nickthedevguy@gmail.com>
2023-04-20 20:38:52 -0700

    added onSubmitHandler and form validation function, added state for errors

--- react-client/src/components/ProductForm.jsx
+++ react-client/src/components/ProductForm.jsx
@@ -12,12 +12,46 @@ const [newProduct, setNewProduct] = useState({
   description: ''
 })
 
+const [errors, setErrors] = useState({})
+
 const {title, price, description} = newProduct
 
 const navigate = useNavigate()
 
 const onChangeHandler = (e) => {
   setNewProduct(prevState => {return {...prevState, [e.target.name]: e.target.value}})
+}
+
+const validateForm = () => {
+  if (title.length < 3 || title.length > 48 ||
+      price.len < 0 ||
+    description.length < 12 || description.length > 384) {
+      return false
+    }
+    return true
+}
+
+const onSubmitHandler = (e) => {
+  e.preventDefault()
+
+  axios.post('http://172.19.216.246:8000/api/products/new', newProduct)
+    .then((res) => {
+      // ! REMOVE BEFORE SUBMITTING ASSIGNMENT
+      console.log(res.data)
+      navigate('/')
+    })
+    .catch(err => setErrors(err.response.data.errors))
+
+    if (validateForm()) {
+      setNewProduct({
+        title: '',
+        price: '',
+        description: ''
+      })
+      setErrors({})
+    }
+
+
 }
 
   return (
