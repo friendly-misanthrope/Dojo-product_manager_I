d3ed7f3ac566ee1e26d51802dbc3d192a1999215
NickBarkerDev <nickthedevguy@gmail.com>
2023-04-20 20:14:27 -0700

    added state object for new product and onChangeHandler for form inputs

--- react-client/src/components/ProductForm.jsx
+++ react-client/src/components/ProductForm.jsx
@@ -6,21 +6,36 @@ import { useNavigate } from 'react-router-dom'
 
 const ProductForm = (props) => {
 
+const [newProduct, setNewProduct] = useState({
+  title: '',
+  price: '',
+  description: ''
+})
+
+const {title, price, description} = newProduct
+
+const navigate = useNavigate()
+
+const onChangeHandler = (e) => {
+  setNewProduct(prevState => {return {...prevState, [e.target.name]: e.target.value}})
+}
+
   return (
     <div className="product-form">
       <form>
         <div className="form-group">
           <label for="title">Title: </label>
-          <input type="text" className="form-control" name="title" />
+          <input type="text" className="form-control" name="title" onChange={onChangeHandler} />
         </div>
         <div className="form-group">
           <label for="price">Price: </label>
-          <input type="text" className="form-control" name="price" />
+          <input type="text" className="form-control" name="price" onChange={onChangeHandler} />
         </div>
         <div className="form-group">
           <label for="description">Description: </label>
-          <input type="text" className="form-control" name="description" />
+          <input type="text" className="form-control" name="description" onChange={onChangeHandler} />
         </div>
+        <input type="submit" value="Submit" />
       </form>
     </div>
   );
