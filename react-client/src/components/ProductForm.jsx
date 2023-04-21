36dc9b9e1397f7363416bef225f85f53d7712cda
NickBarkerDev <nickthedevguy@gmail.com>
2023-04-20 20:08:20 -0700

    imported dependencies for ProductForm, added form JSX for all 3 fields

--- react-client/src/components/ProductForm.jsx
+++ react-client/src/components/ProductForm.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+// TODO: stylesheet here
 
-const ProductForm = () => {
+
+const ProductForm = (props) => {
+
   return (
-    <div>
-      
+    <div className="product-form">
+      <form>
+        <div className="form-group">
+          <label for="title">Title: </label>
+          <input type="text" className="form-control" name="title" />
+        </div>
+        <div className="form-group">
+          <label for="price">Price: </label>
+          <input type="text" className="form-control" name="price" />
+        </div>
+        <div className="form-group">
+          <label for="description">Description: </label>
+          <input type="text" className="form-control" name="description" />
+        </div>
+      </form>
     </div>
   );
 }
